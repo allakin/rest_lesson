45f8ac587cecaebce053a3af3cfc6ebc551c56c8
Pavel <19paul91@mail.ru>
2016-11-19 20:12:34 +0300

    add default files

---
+++ instaFeed/instaFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instaFeed
+//
+//  Created by Павел Анплеенко on 19/11/16.
+//  Copyright © 2016 Pavel Anpleenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
